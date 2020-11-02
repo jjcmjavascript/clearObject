b1cbec7e54cd7acd476645a7c61139fac6163042
jjcmjavascript <47872964+jjcmjavascript@users.noreply.github.com>
2020-11-02 15:51:33 -0300

    Create clear.js
    Crear Objeto copia y limpia, Limpiar Objeto Original

---
+++ clear.js
@@ -0,0 +1,39 @@
+//Copia del Objeto en limpio
+function cleanObject (object, temp = {} ){
+        temp = {};
+
+        Object.entries(object).forEach((item, i) => {
+            const nombre  = item[0];
+            const value = item[1];
+                        if (Array.isArray(value)){ temp[nombre] = [] ; }
+            else if (value && typeof value == 'object'){
+                temp[nombre] = cleanObject(value, {});
+            }
+            else {
+                temp[nombre] = null;
+            }
+        });
+
+
+        return temp;
+}
+
+//Objeto original Limpio
+
+    function cleanObject(object){
+        Objec.entries(object).forEach((item) => {
+            const nombre  = item[0];
+            const value = item[1];
+            if(Array.isArray(value)){
+                object[nombre] = [];
+            }
+            if(value && typeof value == 'object'){
+                object[nombre] = cleanObject(value, {});
+            }
+            else {
+                object[nombre] = null;
+            }
+        });
+
+        return object;
+    },
